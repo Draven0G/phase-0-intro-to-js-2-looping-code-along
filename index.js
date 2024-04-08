2e69ab2adb9caa7ce6dab31312e43aa525943656
Ramsey <browenbroz@gmail.com>
2024-04-08 11:51:48 +0300

    w1ll

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown(num) {
+    if (num <= 0 || !Number.isInteger(num)) {
+        console.log("Please provide a positive integer.");
+        return;
+    }
+
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+// Example usage:
+countDown(10);
